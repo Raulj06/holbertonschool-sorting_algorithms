26bd6c74ac1acbf0fe90354fb50f4b2093b3fa48
Raulj06 <5015@holbertonschool.com>
2022-08-29 07:14:19 +0000

    fixing

--- sort.h
+++ sort.h
@@ -28,5 +28,5 @@ void quick_sort(int *array, size_t size);
 void quickSort(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
 void swap(int *array, int i, int j, size_t size);
-
+void selection_sort(int *array, size_t size);
 #endif
